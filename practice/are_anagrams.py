72846c183e1b73ff39553f3b56dc4f2a4f957c8c
Adstefnum <bauerstevenson@gmail.com>
2022-03-31 16:16:49 +0300

    adding complexities

--- practice/are_anagrams.py
+++ practice/are_anagrams.py
@@ -1,9 +1,14 @@
+#O(n) space from two O(n) stored dicts
+#O(n) time from two O(n) traversing both strings and then dicts have O(1) 
+#lookup times
 def are_anagrams_1(s1,s2):
   if len(s1)!= len(s2):
     return False
     
   return {i:s1.count(i) for i in s1} == {i:s2.count(i) for i in s2}
 
+#O(n) time from 2 O(n) traversing of both strings 
+#O(n) space as Counter returns a dict
 def are_anagrams_2(s1,s2):
   if len(s1)!= len(s2):
     return False
@@ -12,6 +17,8 @@ def are_anagrams_2(s1,s2):
   return Counter(s1) == Counter(s2)
 
 #They are lexicographically the same text
+#O(nlog(n)) time as the sorting algorithm takes O(nlog(n))
+#O(1) space as nothing is stored since we sort the string in itself
 def are_anagrams_3(s1,s2):
   if len(s1)!= len(s2):
     return False
