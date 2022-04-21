00944949caca27558f3b1fd352aa5125ee4fb0a9
Stephen Adesina <bauerstevenson@gmail.com>
2022-04-22 01:02:03 +0300

    how to get its index in the unsorted original arr

--- practice/binary_search.py
+++ practice/binary_search.py
@@ -1,18 +1,24 @@
-#O(log(n)) time| O(1) space
-def binarySearch(array, target):
-	array.sort()
+from typing import Any
 
-	left = 0
-	right = len(array)-1
-
-	while left <= right:
-		middle = (left + right)//2
-		if array[middle] == target:
-			return middle
-
-		elif array[middle] > target:
-			right = middle -1
-
-		elif array[middle] < target:
-			left  = middle + 1
-
+#O(log(n) time | O(1) space
+def binary_search(array:list,target:Any)->Any:
+    
+    array.sort()
+    left,right=0,len(array)-1
+    
+    while left <= right:
+        mid = (left+right)//2
+        
+        if array[mid]==target:
+            return mid
+        
+        elif array[mid]<target:
+            left = mid + 1
+            
+        elif array[mid]> target:
+            right = mid - 1
+            
+    return None
+    
+print(binary_search([1,2,4,5,2.6,7.3,8,3,7,],11))
+print(binary_search([1,2,4,5,2.6,7.3,8,3,7,],2.6))
