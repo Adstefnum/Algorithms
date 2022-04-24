52422447fcda41cc8e0a8235f548815ec69422c2
Stephen Adesina <bauerstevenson@gmail.com>
2022-04-24 04:37:42 +0300

    Update quicksort.py

--- practice/quicksort.py
+++ practice/quicksort.py
@@ -1,39 +1,42 @@
-def partition(arr, left, right, pivot):
-
-    while left <=right:
-
-        while arr[left] < pivot:
-            left += 1
-
-        while arr[right] > pivot:
-            right -= 1
-
-        if left <= right:
-            arr[left],arr[right] = arr[right],arr[left]
-            left += 1
-            right -= 1
-    return left
-
 #Average:O(nlog(n)) time, Worst: O(n^2) time in case of choosing a bad pivot e.g the lowest element
 #Average: O(log(n), Worst:O(n) space
-def quicksort(arr, left,right):
+from typing import Any
+
+def quickSort(array:list,left:int,right:int)->list:
     
-    if len(arr)  == 1:
-        return arr
-
+    def partition(array:list,left:int,right:int,pivot:Any)->int:
+        
+        while left <= right:
+            
+            while array[left] < pivot:
+                left += 1
+                
+            while array[right] > pivot:
+                right -= 1
+                
+            if left <= right:
+                array[left],array[right]=array[right],array[left]
+                right -= 1
+                left += 1
+                
+        return left
+        
+    if len(array)==1:
+        return array
+    
     if left < right:
-
-        pivot_index = (left+right)//2
-        pivot = arr[pivot_index]
-        idx = partition(arr,left,right,pivot)
-        quicksort(arr,left,idx-1)
-        quicksort(arr,idx,right)
-
-
-
+        pivot = array[(left+right)//2]
+        idx = partition(array,left,right,pivot)
+        quickSort(array,left,idx-1)
+        quickSort(array,idx,right)
+        
+    return array
+        
 arr = [2,56,2,8,4,9,5,74,8,35,64,6,3,65,745]
 left,right = 0, len(arr)-1
-quicksort(arr,left,right)
+print(quickSort(arr,left,right))
+      
+        
 print(arr)
 
 
