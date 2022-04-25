aed2dd4b05b1d43329204a97d5fc6d6c06d57fc9
Stephen Adesina <bauerstevenson@gmail.com>
2022-04-25 22:59:34 +0300

    Update word_arithmetic.py

--- practice/word_arithmetic.py
+++ practice/word_arithmetic.py
@@ -21,23 +21,28 @@ def word_arithmetic(op:str)->str:
         "-":"minus"
     }
     
-    #list of tokens
-    tokens = [] 
-    ret = 0
+    #list of pre_tokens
+    pre_tokens = [] 
+    ret = ""
    
     for key,value in tokens_lookup.items():
         res = re.search(value,op)
         #try using eval statement to calculate the answer in a += manner
         if res:
-            tokens.append([key,res.start()])
-            
+            pre_tokens.append([str(key),res.start()])
+    #O(len(op)log(len(op) if we traverse over each letter?
+    pre_tokens.sort(key=lambda x: x[1])
+    tokens = [i[0] for i in pre_tokens]
+    ret = int(eval("".join(tokens)))
     
     if ret >0:
         rex = "negative"+tokens_lookup[ret]
     else:
         rex = tokens_lookup[ret]
     
-    return tokens
+    return rex
 
-print(word_arithmetic("onepluseight"))
+#need to handle taking in duplicate values e.g two ones in onepluseightminusonezero
+#need to break back up into tokens to return right answer e.g -11= negative one one
+print(word_arithmetic("onepluseightminustwozero"))
     
