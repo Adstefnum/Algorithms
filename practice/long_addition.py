99538e0349251441a31e95aa9ff12a8b5a21400b
Stephen Adesina <bauerstevenson@gmail.com>
2022-04-28 12:47:25 +0300

    Update long_addition.py

--- practice/long_addition.py
+++ practice/long_addition.py
@@ -2,57 +2,20 @@
 #numbers in strings
 #"11" "22" = "33"
 #don't convert to int and dont support decimal and negatives
+#assume that a letter or empty space will not be added
 
-def string_add_2(num1:str, num2:str)->str:
-    ret = []
-    a,b = len(num1)-1,len(num2)-1
-    if len(num1)>=len(num2):
-        n = len(num1)
-    elif len(num1)<len(num2):
-        n = len(num2)
-        
-    for i in range(n):
-        sum_ = str(eval(f"{num1[a-i]} + {num2[b-i]}"))
-        if int(sum_) > 9:
-            rem = str(eval(f"{sum_}-10"))
-            if "#1" in ret:
-                cr_idx = ret.index("#1")
-                ret[cr_idx] = str(eval(f"{rem}+1"))
-            else:
-                ret.append(rem)
-                ret.append("#1")
-                
-        else:
-            if "#1" in ret:
-                cr_idx = ret.index("#1")
-                ret[cr_idx] = str(eval(f"{sum_}+1"))
-            else:
-                ret.append(sum_)
-            print(ret)
-            
-    return "".join(ret[::-1])
-
-  #edgecases
-  # 1 + 99
-  # 1009 +1009
-  # 2019 + 2019
-print(string_add_2("1","99"))
-
-
-#num1 num2
-#numbers in strings
-#"11" "22" = "33"
-#don't convert to int and dont support decimal and negatives
-
+#O(num1+num2) time as we have to loop over the both strings completely
+#O(n+x) space as we always have to store the result which is of lenght equal to
+#original numbers but may have a x value more than the input lenght after addition
 def string_add_2(num1:str, num2:str)->str:
     
-    #make num 2 the bigger number
+    #make num 2 the bigger number instead of checking for lenght
     if len(num1)>len(num2):
         temp = num1
         num1 = num2
         num2 = temp
         
-    ret = ""
+    rex = []
     a,b = len(num1)-1,len(num2)-1
     diff = abs(a-b)
     carry = 0
@@ -61,24 +24,29 @@ def string_add_2(num1:str, num2:str)->str:
     #diff enables us to reach the end of the larger string
     for i in range(a,-1,-1):
         sum_ = str(eval(f"{num1[i]} + {num2[i+diff]} + {carry}"))
-        ret += str(eval(f"{sum_}%10"))
+        ret = str(eval(f"{sum_}%10"))
+        rex.append(ret)
         carry = str(eval(f"{sum_}//10"))
     
-    for j in range(diff,-1,-1):
-        sum_ = str(eval(f"{num2[j]} + {carry}"))
-        ret += str(eval(f"{sum_}%10"))
-        carry = str(eval(f"{sum_}//10"))
-
+    #if the difference between the length is not zero add the other numbers to the list
+    if diff!=0:
+        for j in range(diff,-1,-1):
+            sum_ = str(eval(f"{num2[j]} + {carry}"))
+            ret = str(eval(f"{sum_}%10"))
+            rex.append(ret)
+            carry = str(eval(f"{sum_}//10"))
+    
+    #if at the end of the addition there is still a carry left then add it to the last element in the list
     if carry:
-        to_add = str(eval(f"{carry}+{ret[-1]}"))
-        print(to_add)
-        ret = ret[:-1] + to_add
-            
-            
-    return ret[::-1]
+        rex[-1] = str(eval(f"{rex[-1]} + {carry}"))
+        
+        
+    #join the string and reverse it as we added from the back to the front        
+    return "".join(rex)[::-1]
 
   #edgecases
   # 1 + 99
   # 1009 +1009
   # 2019 + 2019
-print(string_add_2("1","99"))
+  # 54221350301866419569709863044208 +94919791112382438694026971412463
+print(string_add_2("54221350301866419569709863044208","94919791112382438694026971412463"))
